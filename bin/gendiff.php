d5ddba408517ee5119b7bcf435f44e44be1d85bb
itaopro <malchikov.ne@gmail.com>
2022-04-12 12:16:29 +0300

    add gendiff

---
+++ bin/gendiff.php
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+<?php
+
+gendiff -h
+
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+
+Options:
+  -h --help                     Show this screen
+  -v --version                  Show version
