fd1eb5d07241fda220e1604615805f5267371f58
Nikolai Malchikov <malchikov.ne@gmail.com>
2022-05-31 22:52:39 +0300

    update

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -1,14 +1,24 @@
 <?php
+
 namespace Gendiff\Code\Tests;
 
 use PHPUnit\Framework\TestCase;
 
-use function Src\Differ;
+use function Src\Differ\gendiff;
 
 class GenDiffTest extends TestCase
 {
-public function testGendiff()
-{
-    $this->assertStringEqualsFile('{"- follow":"false","host":"hexlet.io","- proxy":"123.234.53.22","- timeout":50,"+ timeout":20,"+ verbose":"true"}', gendiff($file1, $file2));
-}
+    public function testGendiff($file1, $file2)
+    {
+        $this->assertStringEqualsFile(
+            '{"- follow":"false",
+            "host":"hexlet.io",
+            "- proxy":"123.234.53.22",
+            "- timeout":50,
+            "+ timeout":20,
+            "+ verbose":
+            "true"}',
+            gendiff($file1, $file2)
+        );
+    }
 }
